72924198b6e7c6a5c06164e97d322f0046e46a17
Brett Johnson <brett.johnson@redhat.com>
2020-11-06 12:27:10 +1100

    initial

---
+++ builds/s2i-time/main.go
@@ -0,0 +1,16 @@
+// +build go1.14
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	for i := 0; i < 600; i++ {
+		fmt.Printf("Current Unix Time: %v\n", time.Now().Unix())
+		time.Sleep(1 * time.Second)
+	}
+}
